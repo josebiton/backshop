6cdfe4bf0b524d53485c257f19862109ea5908d3
josebiton <95506460+josebiton@users.noreply.github.com>
2024-06-23 22:49:24 -0500

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'database-bitoncito.cqqlkrymbtcy.us-east-1.rds.amazonaws.com',
+        'username'     => 'admin',
+        'password'     => 'jnbb11jsm',
+        'database'     => 'shopmarket',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
